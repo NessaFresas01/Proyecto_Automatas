e06935822120bef735ce06080bfd1426a30aa762
Vanessa Márquez <vanessaf.maf@gmail.com>
2025-03-13 18:11:05 -0600

    06:09 p.m - Se agrego el FileChooser.
    Dentro de control se creo la parte lógica para que el programa abra un fileChooser para seleccionar el archivo deseado.
    
    Co-Authored-By: skeleer <197271244+skeleer@users.noreply.github.com>

--- Proyecto_Automatas/src/proyecto_automatas/control/Control.java
+++ Proyecto_Automatas/src/proyecto_automatas/control/Control.java
@@ -3,8 +3,63 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto_automatas.control;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import proyecto_automatas.vista.Ventana;
 
 /**
  *
  * @author Vanessa, Adrián
  */
+
+  public class Control { // Define la clase Control.
+    Ventana v; // Declara una variable de tipo Ventana.
+
+    public Control(Ventana v) { // Constructor de la clase Control que recibe un objeto Ventana.
+        this.v = v; // Asigna el objeto Ventana recibido a la variable v.
+    }
+
+    // Método para abrir archivo con file chooser.
+    public void abrirArchivo() { 
+        JFileChooser chooser = new JFileChooser(); // Crea un objeto JFileChooser para seleccionar archivos.
+        chooser.setDialogTitle("Seleccionar Archivo"); // Establece el título del diálogo.
+        String path = null; // Declara una variable para almacenar la ruta del archivo.
+        int value = chooser.showOpenDialog(v); // Muestra el diálogo de selección de archivos y guarda el resultado.
+        
+        if (value == JFileChooser.APPROVE_OPTION) { // Verifica si se seleccionó un archivo.
+            path = chooser.getSelectedFile().getAbsolutePath(); // Obtiene la ruta del archivo seleccionado.
+            v.getLblArchivo().setText(path); // Muestra la ruta de acceso en la etiqueta de la ventana.
+        }
+        
+        if (path == null) { // Verifica si no se seleccionó correctamente un archivo.
+            JOptionPane.showMessageDialog(v, "No fue seleccionado correctamente"); // Muestra un mensaje de error.
+            return; // Termina la ejecución del método.
+        }
+        
+        leerArchivo(path); // Llama al método leerArchivo con la ruta del archivo.
+    }
+
+    public void leerArchivo(String archivo) { 
+        try {            
+            BufferedReader br = new BufferedReader(new FileReader(archivo)); 
+            // Crea un BufferedReader para leer el archivo. FileReader es una clase anónima.
+            String linea; // Declara una variable para almacenar cada línea del archivo.
+            
+            while ((linea = br.readLine()) != null) { // Lee cada línea del archivo hasta que no haya más.
+                v.getTxtContenido().append(linea + "\n"); // Agrega la línea leída al contenido del JTextArea en la ventana.
+            }
+        } catch (IOException e) { // Captura excepciones de tipo IOException.
+            System.err.print("Error al leer el archivo: " + e.getMessage()); // Muestra un mensaje de error en la consola.
+        }
+    }    
+}
+
+  
+
+    
+            
+        
+    
--- Proyecto_Automatas/src/proyecto_automatas/vista/Ventana.java
+++ Proyecto_Automatas/src/proyecto_automatas/vista/Ventana.java
@@ -3,60 +3,114 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package proyecto_automatas.vista;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import proyecto_automatas.control.Control;
 
 /**
  *
  * @author Vanessa, Adrián
  */
 public class Ventana extends javax.swing.JFrame {
+    Control c;
 
     /**
      * Creates new form Ventana
      */
     public Ventana() {
         initComponents();
+         c = new Control(this);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jRadioButtonMenuItem1 = new javax.swing.JRadioButtonMenuItem();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        TxtSalida = new javax.swing.JTextArea();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextAreaCodigo = new javax.swing.JTextArea();
-        jScrollPaneMensaje = new javax.swing.JScrollPane();
+        txtContenido = new javax.swing.JTextArea();
+        jlblArchivo = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jmnuArchivo = new javax.swing.JMenu();
-        jmnuAbrir = new javax.swing.JMenu();
-        jmnuLimpiar = new javax.swing.JMenu();
-        jmnuSalir = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+        mnuOpen = new javax.swing.JMenuItem();
+        mnuClean = new javax.swing.JMenuItem();
+        jMenuSalir = new javax.swing.JMenuItem();
         jmnuControl = new javax.swing.JMenu();
-        jmnuLexico = new javax.swing.JMenu();
+        mnuLexico = new javax.swing.JMenuItem();
+
+        jMenu1.setText("jMenu1");
+
+        jMenu2.setText("jMenu2");
+
+        jMenu3.setText("jMenu3");
+
+        jCheckBoxMenuItem1.setSelected(true);
+        jCheckBoxMenuItem1.setText("jCheckBoxMenuItem1");
+
+        jMenuItem1.setText("jMenuItem1");
+
+        jRadioButtonMenuItem1.setSelected(true);
+        jRadioButtonMenuItem1.setText("jRadioButtonMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Ventana Principal");
         setResizable(false);
 
-        jTextAreaCodigo.setColumns(20);
-        jTextAreaCodigo.setRows(5);
-        jScrollPane1.setViewportView(jTextAreaCodigo);
+        TxtSalida.setEditable(false);
+        TxtSalida.setColumns(20);
+        TxtSalida.setRows(5);
+        jScrollPane2.setViewportView(TxtSalida);
 
-        jmnuArchivo.setText("Archivo");
+        txtContenido.setColumns(20);
+        txtContenido.setRows(5);
+        jScrollPane1.setViewportView(txtContenido);
 
-        jmnuAbrir.setText("Abrir");
-        jmnuArchivo.add(jmnuAbrir);
+        jlblArchivo.setText("Archivo : ");
 
-        jmnuLimpiar.setText("Limpiar");
-        jmnuArchivo.add(jmnuLimpiar);
+        jMenu4.setText("Archivo");
 
-        jmnuSalir.setText("Salir");
-        jmnuArchivo.add(jmnuSalir);
+        mnuOpen.setText("Abrir");
+        mnuOpen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuOpenActionPerformed(evt);
+            }
+        });
+        jMenu4.add(mnuOpen);
 
-        jMenuBar1.add(jmnuArchivo);
+        mnuClean.setText("Limpiar");
+        mnuClean.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuCleanActionPerformed(evt);
+            }
+        });
+        jMenu4.add(mnuClean);
+
+        jMenuSalir.setText("Salir");
+        jMenuSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuSalirActionPerformed(evt);
+            }
+        });
+        jMenu4.add(jMenuSalir);
+
+        jMenuBar1.add(jMenu4);
 
         jmnuControl.setText("Control");
 
-        jmnuLexico.setText("Lexico");
-        jmnuControl.add(jmnuLexico);
+        mnuLexico.setText("Lexico");
+        mnuLexico.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuLexicoActionPerformed(evt);
+            }
+        });
+        jmnuControl.add(mnuLexico);
 
         jMenuBar1.add(jmnuControl);
 
@@ -69,33 +123,85 @@ public class Ventana extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
-                    .addComponent(jScrollPaneMensaje))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 452, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 452, Short.MAX_VALUE)
+                    .addComponent(jlblArchivo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPaneMensaje, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jlblArchivo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 7, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    
+    //CÓDIGO PARA ABRIR ARCHIVO CON UN JFILECHOOSER.
+    private void mnuOpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuOpenActionPerformed
+        c.abrirArchivo();
+    }//GEN-LAST:event_mnuOpenActionPerformed
 
+    private void mnuCleanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuCleanActionPerformed
+       
+    }//GEN-LAST:event_mnuCleanActionPerformed
+
+    private void mnuLexicoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuLexicoActionPerformed
+
+
+    }//GEN-LAST:event_mnuLexicoActionPerformed
+
+    private void jMenuSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuSalirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuSalirActionPerformed
+    public JLabel getLblArchivo() {
+        return jlblArchivo;
+    }
+
+    public JTextArea getTxtSalida() {
+        return TxtSalida;
+    }
+
+    public void setTxtSalida(JTextArea TxtSalida) {
+        this.TxtSalida = TxtSalida;
+    }
+
+    public JTextArea getTxtContenido() {
+        return txtContenido;
+    }
+
+    public void setTxtContenido(JTextArea txtContenido) {
+        this.txtContenido = txtContenido;
+    }
+    
+  
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea TxtSalida;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuSalir;
+    private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPaneMensaje;
-    private javax.swing.JTextArea jTextAreaCodigo;
-    private javax.swing.JMenu jmnuAbrir;
-    private javax.swing.JMenu jmnuArchivo;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel jlblArchivo;
     private javax.swing.JMenu jmnuControl;
-    private javax.swing.JMenu jmnuLexico;
-    private javax.swing.JMenu jmnuLimpiar;
-    private javax.swing.JMenu jmnuSalir;
+    private javax.swing.JMenuItem mnuClean;
+    private javax.swing.JMenuItem mnuLexico;
+    private javax.swing.JMenuItem mnuOpen;
+    private javax.swing.JTextArea txtContenido;
     // End of variables declaration//GEN-END:variables
+
+  
+    
 }
