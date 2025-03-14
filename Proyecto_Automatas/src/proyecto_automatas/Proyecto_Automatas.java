5b498e1d62b0d2cbf2df3784b83e03c227838bfc
Vanessa Márquez <vanessaf.maf@gmail.com>
2025-03-14 17:19:30 -0600

    14/03/2025 - Revisión
    Revisión de JFileChooser
    
    Co-Authored-By: skeleer <197271244+skeleer@users.noreply.github.com>

---
+++ Proyecto_Automatas/src/proyecto_automatas/Proyecto_Automatas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_automatas;
+import proyecto_automatas.vista.Ventana;
+
+/**
+ *
+ * @author Vanessa, Adrián
+ */
+//Revisión marzo 14
+public class Proyecto_Automatas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana v = new Ventana(); //Instanciamos la clase Ventana.
+        v.setVisible(true); //Hace visible la ventana.
+        v.setLocationRelativeTo(null); //Coloca la ventana siempre al centro de la pantalla.
+        }
+    }
+
+
