d4889692eeea5ebb2d8496bc3a00f9483a217129
Vanessa Márquez <vanessaf.maf@gmail.com>
2025-06-03 18:40:27 -0600

    Avance en la clase AnaSintaxis 6:40 p.m
    Co-Authored-By: skeleer <197271244+skeleer@users.noreply.github.com>

--- Proyecto_Automatas/src/proyecto_automatas/sintaxis/AnaSintaxis.java
+++ Proyecto_Automatas/src/proyecto_automatas/sintaxis/AnaSintaxis.java
@@ -53,6 +53,7 @@ public class AnaSintaxis {
         }
 
     }
+
     /**
      * Regla <Programa> -> <Bloque>. Se espera un punto (.) al final del
      * programa.
@@ -123,6 +124,7 @@ public class AnaSintaxis {
 
         }
     }
+
     // <bIdentificador> -> id | id , <bIdentificador>
     private static void bIdentificador() {
         if (tokenActual == 4) { //4 corresponde a id
@@ -148,99 +150,178 @@ public class AnaSintaxis {
             }
         }
     }
-    
+
     //Chaguitos para q jale :D
-    
     //<bProcedimiento> -> Proced id ; <Bloque> ; | ∅
-    private static void bProcedimiento(){
-        
+    private static void bProcedimiento() {
+        if (tokenActual == 9) { //El 9 corresponde a const
+            avanzarToken();
+            if (tokenActual == 4) { //4 corresponde a id
+                avanzarToken();
+                if (tokenActual == 30) { //30 es para ;
+                    avanzarToken();
+                    Bloque();
+                    if (tokenActual == 30) { //30 es para ;
+                        avanzarToken();
+                    } else {
+                        throw new RuntimeException("Se esperaba ';' después del bloque del procedimiento");
+                    }
+                } else {
+                    throw new RuntimeException("Se esperaba ';' después del bloque del indentificador");
+                }
+            } else {
+                throw new RuntimeException("Se esperaba un identificador despu[es de proced");
+            }
+        }
     }
-     // <Proposicion>
-    private static void Proposicion(){
-        
+
+    // <Proposicion>
+    private static void Proposicion() {
+
+        //Se supone que al entrar el . se finaliza el programa
+        //el -1 verifica que ya no hay tokens que analizar
+        //No terminal 
+        if (tokenActual != 120 && tokenActual != -1) {
+            //Si aun hay tokens entonces entra de nuevo a pMultiplo();
+            pMultiplo();
+        }
+
     }
-     // <pMultiplo> -> 
-    private static void pMultiplo(){
-        
+
+    // <pMultiplo> -> 
+    private static void pMultiplo() {
+        switch (tokenActual) {
+            case 65: // {
+                pPropisicion();
+                break;
+            case 4: // id
+                pAsignacion();
+                break;
+            case 8: //Print
+                pPrint();
+                break;
+            case 6: //input
+                pInput();
+                break;
+            case 2: //exec
+                pExec();
+                break;
+            case 5: //if
+                pCondicion();
+                break;
+            case 11: //while
+                pWhile();
+                break;
+            case 3: //for
+                pFor();
+                break;
+            default:
+                throw new RuntimeException("Proposicion no reconocidaF");
+        }
     }
+
     // <pProposicion> -> { <pSecuencia> }
-    private static void pProposicion(){
-        
+    private static void pProposicion() {
+        if (tokenActual == 65) { // {
+            avanzarToken();
+            pSecuencia();
+            if (tokenActual == 70) { // }
+                avanzarToken();
+            } else {
+                throw new RuntimeException("Se esperaba '}' para cerrar la proposición");
+
+            }
+        } else {
+            throw new RuntimeException("Se esperaba '{' para iniciar proposición");
+
+        }
     }
+
     // <pSecuencia> -> <Proposicion> <pSecuenciaA>
-    private void pSecuencia(){
-        
+    private static void pSecuencia() {
+
     }
-     // <pSecuenciaA> -> ∅ | ; <pSecuencia>
-    private static void pSecuenciaA(){
-        
+    // <pSecuenciaA> -> ∅ | ; <pSecuencia>
+
+    private static void pSecuenciaA() {
+
     }
+
     // <pAsignacion> -> id = <Expresion>
-    private static void pAsignacion(){
-        
+    private static void pAsignacion() {
+
     }
+
     // <pPrint> -> print <pDato>
-    private static void pPrint(){
-        
+    private static void pPrint() {
+
     }
+
     // <pInput> -> input id
-    private static void pInput(){
-        
+    private static void pInput() {
+
     }
+
     // <pExec> -> exec id
-    private static void pExce(){
-        
+    private static void pExce() {
+
     }
+
     // <pCondicion> -> if <cCondicion> : <Proposicion>
-    private static void pCondicion(){
-        
+    private static void pCondicion() {
+
     }
-     // <pFor> -> for id = <Expresion> <pFlechas> <Expresion> : <Proposicion>
-    private static void pFor(){
-        
+    // <pFor> -> for id = <Expresion> <pFlechas> <Expresion> : <Proposicion>
+
+    private static void pFor() {
+
     }
-     // <pFlechas> -> -> | <-
-    private static void pFlechas(){
-        
+    // <pFlechas> -> -> | <-
+
+    private static void pFlechas() {
+
     }
+
     // <pDato> -> id | num
-    private static void pDato(){
-        
+    private static void pDato() {
+
     }
+
     // <cCondicion> -> <Expresion> <cMultiplo> <Expresion>
-    private static void cCondicion(){
+    private static void cCondicion() {
         Expresion();
         cMultiplo();
         Expresion();
     }
+
     // <cMultiplo> -> == | <> | < | > | <= | >=
-    private static void cMultiplo(){
-        
+    private static void cMultiplo() {
+
     }
-    
-     // <Expresion> -> <Termino> <eExpresionA>
-    private static void Expresion(){
-        
+
+    // <Expresion> -> <Termino> <eExpresionA>
+    private static void Expresion() {
+
     }
+
     // <eExpresionA> -> ∅ | <eSignos> <Expresion>
-    private static void eExpresionA(){
-        
+    private static void eExpresionA() {
+
     }
-     // <Termino> -> <Factor> <tTerminoA>
-    private static void Termino(){
-        
+    // <Termino> -> <Factor> <tTerminoA>
+
+    private static void Termino() {
+
     }
-     // <tTerminoA> -> ∅ | <tSignos> <Termino>
-    private static void tTerminoA(){
-        
+    // <tTerminoA> -> ∅ | <tSignos> <Termino>
+
+    private static void tTerminoA() {
+
     }
-     // <Factor> -> <fFactor>
-    private static void Factor(){
-        
+    // <Factor> -> <fFactor>
+
+    private static void Factor() {
+
     }
-    
-    
-    
-    
-    
+
 }
